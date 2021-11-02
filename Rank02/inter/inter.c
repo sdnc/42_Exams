1821cac594205f4759c29706f14de750ec042272
sdnc <dza.sara@gmail.com>
2021-11-02 23:26:01 +1030

    021121

---
+++ Rank02/inter/inter.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+
+void	putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	find_dup(char *s, char c, size_t idx)
+{
+	size_t	i = 0;
+
+	while (i < idx)
+	{
+		if (s[i] == s[idx])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	size_t	i;
+	size_t	j;
+	
+	i = 0;	
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			if (!find_dup(av[1], av[1][i], i))
+			{
+				j = 0;
+				while (av[2][j])
+				{
+					if (av[1][i] == av[2][j])
+					{
+						putchar(av[1][i]);
+						break ;
+					}
+					j++;
+				}	
+			}
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
