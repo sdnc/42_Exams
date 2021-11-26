88f95b85d6c8236b4ee8438a1395ca9332722d88
sdnc <dza.sara@gmail.com>
2021-11-26 11:23:54 +1030

    261121

--- Rank02/get_next_line/get_next_line.c
+++ Rank02/get_next_line/get_next_line.c
@@ -1,34 +1,74 @@
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
 
-int ft_strlen(const char *str)
+int ft_strlen(const char *s)
 {
     int i = 0;
 
-    while (str[i])
+    while (s[i])
         i++;
     return (i);
 }
 
-char    *ft_substr(char *str, unsigned int start, int len)
+char    *ft_strjoin(const char *s1, const char *s2)
 {
-    char    *substr;
+    char    *str;
     int     i = 0;
     int     j = 0;
 
-    substr = (char *)malloc(sizeof(char) * (len + 1));
+    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+    if (!str)
+        return (NULL);
+    while (s1[i])
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        str[i + j] = s2[j];
+        j++;
+    }
+    str[i + j] = '\0';
+    return (str);
+}
+
+char    *ft_substr(const char *str, int start, int len)
+{
+    char *substr;
+    int i = 0;
+    int j = 0;
+
+    substr = (char *)malloc(sizeof(char) * len + 1);
     if (!substr)
         return (NULL);
     while (str[i])
     {
         if (i >= start && j < len)
             substr[j++] = str[i];
-        i++;
+        i++; 
     }
     substr[j] = '\0';
     return (substr);
+}
+
+char    *ft_strdup(const char *str)
+{
+    char    *dup;
+    int     i = 0;
+
+    dup = (char *)malloc(sizeof(char) * ft_strlen(str) + 1);
+    if (!dup)
+        return (NULL);
+    while (str[i])
+    {
+        dup[i] = str[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
 }
 
 char    *ft_strchr(const char *s, int c)
@@ -41,60 +81,19 @@ char    *ft_strchr(const char *s, int c)
             return ((char *)s);
         s++;
     }
-    return (0);
-}
-
-char    *ft_strdup(const char *s)
-{
-    char    *dup;
-    int     i = 0;
-
-    dup = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-    if (!dup)
-        return (NULL);
-    while (s[i])
-    {
-        dup[i] = s[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
-}
-
-char    *ft_strjoin(const char *s1, const char *s2)
-{
-    char    *str;
-    int     i = 0;
-    int     j = 0;
-
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (!str)
-        return (0);
-    while (s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }    
-    while (s2[j])
-    {
-        str[i + j] = s2[j];
-        j++;
-    }
-    str[i + j] = '\0';
-    return (str);
+    return (NULL);
 }
 
 static char *read_n_append(int fd, char *buffer, char *remainder)
 {
-    int      bytes_read;
+    int bytes_read = 1;
     char    *temp;
 
-    bytes_read = 1;
     while (bytes_read != 0)
     {
         bytes_read = read(fd, buffer, BUFFER_SIZE);
-        if (bytes_read == -1)
-            return (NULL);
+        if (bytes_read == -1)   
+            return NULL;
         if (bytes_read == 0)
             break ;
         buffer[bytes_read] = '\0';
@@ -110,7 +109,7 @@ static char *read_n_append(int fd, char *buffer, char *remainder)
     return (remainder);
 }
 
-static char *update_next_line(char *line)
+static char *update_line(char *line)
 {
     char    *remainder;
     int     idx = 0;
@@ -119,8 +118,8 @@ static char *update_next_line(char *line)
         idx++;
     if (line[idx] == '\0' || line[1] == '\0')
         return (NULL);
-    remainder = ft_substr(line, idx + 1, ft_strlen(line) - 1);
-    if (*remainder == '\0')
+    remainder = ft_substr(line, idx + 1, ft_strlen(line) - idx);
+    if (!(*remainder))
     {
         free(remainder);
         remainder = NULL;
@@ -128,6 +127,7 @@ static char *update_next_line(char *line)
     line[idx + 1] = '\0';
     return (remainder);
 }
+
 char    *get_next_line(int fd)
 {
     char        *line;
@@ -135,19 +135,18 @@ char    *get_next_line(int fd)
     static char *remainder;
 
     if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE * 1));
+        return (0);
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
     if (!buffer)
         return (NULL);
     line = read_n_append(fd, buffer, remainder);
     free(buffer);
     buffer = NULL;
     if (!line)
-        return (NULL);
-    remainder = update_next_line(line);
+        return (0);
+    remainder = update_line(line);
     return (line);
 }
-
 
 int main(void)
 {
